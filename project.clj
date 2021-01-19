2498022afbd4f78b487815ef8d7e804fe6e15369
Malik Kennedy <mksybr@gmail.com>
2021-01-19 10:59:42 -0800

    chore: dependencies for sql driver

--- project.clj
+++ project.clj
@@ -35,6 +35,7 @@
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.postgresql/postgresql "42.2.18"]
+                 [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.8.6"]
                  [org.webjars.npm/bulma "0.9.1"]
                  [org.webjars.npm/material-icons "0.3.1"]
                  [org.webjars/webjars-locator "0.40"]
