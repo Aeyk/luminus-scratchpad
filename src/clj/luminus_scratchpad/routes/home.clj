f88d5959a650ab45166a7f58bf65aaeb7cc224b8
Malik Kennedy <mksybr@gmail.com>
2021-01-19 14:28:54 -0800

    feat: Simple user registration from REST

--- src/clj/luminus_scratchpad/routes/home.clj
+++ src/clj/luminus_scratchpad/routes/home.clj
@@ -15,6 +15,21 @@
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/actions/register"
+    {:post
+     {:handler
+      (fn [req]
+        (let [user (-> req
+                       :body-params
+                       (dissoc :permissions))]
+          (try
+            (do
+              (db/add-user! user)
+              {:status 201
+               :body   {:status "OK"}})
+            (catch clojure.lang.ExceptionInfo e
+              {:status 401
+               :body   {:status (str e)}}))))}}]
    ["/docs" {:get (fn [_]
                     (-> (response/ok (-> "docs/docs.md" io/resource slurp))
                         (response/header "Content-Type" "text/plain; charset=utf-8")))}]])
