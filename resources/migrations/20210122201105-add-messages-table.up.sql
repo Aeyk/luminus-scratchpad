a0f2ec8a6d79f6df134b804c3c1f674de6aaf60f
Malik Kennedy <mksybr@gmail.com>
2021-01-22 12:19:46 -0800

    bugfix: add content column to messages table

--- resources/migrations/20210122201105-add-messages-table.up.sql
+++ resources/migrations/20210122201105-add-messages-table.up.sql
@@ -2,8 +2,9 @@ CREATE TABLE messages
   (
     id INT GENERATED ALWAYS AS IDENTITY,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    from_user_id uuid NOT NULL,
 
-    FOREIGN KEY (from_user_id) REFERENCES users(id) ON DELETE CASCADE
+    from_user_id uuid NOT NULL,
+    FOREIGN KEY (from_user_id) REFERENCES users(id) ON DELETE CASCADE,
+    content text NOT NULL
 
   );
