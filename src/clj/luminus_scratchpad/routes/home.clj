5e72d90fe3b1fe73ce9461ef88c1e9e08c3a4d4d
Malik Kennedy <mksybr@gmail.com>
2021-01-23 06:05:18 -0800

    feat: Add event listener

--- src/clj/luminus_scratchpad/routes/home.clj
+++ src/clj/luminus_scratchpad/routes/home.clj
@@ -1,7 +1,12 @@
 (ns luminus-scratchpad.routes.home
   (:require
    [luminus-scratchpad.layout :as layout]
+   [clojure.tools.logging :as log]
+   [org.httpkit.server
+    :refer [send! with-channel on-close on-receive]]
+   [luminus.http-server :as http]
    [luminus-scratchpad.db.core :as db]
+   [mount.core :as mount]
    [cheshire.core :as json]
    [luminus-scratchpad.jwt :as jwt]
    [clojure.java.io :as io]
@@ -10,9 +15,32 @@
    [buddy.hashers :as hashers]
    [buddy.auth :as auth]
    [buddy.auth.accessrules :as acl]
-   [ajax.core :refer [GET POST]]
+   [ajax.core :refer [GET POST]]   
    [ring.util.http-response :as response]))
 
+;; * WebSocket Utils  etc
+(defonce channels (atom #{}))
+
+(defn connect! [channel]
+  (log/info "channel open")
+  (swap! channels conj channel))
+
+(defn disconnect! [channel status]
+  (log/info "channel closed:" status)
+  (swap! channels #(remove #{channel} %)))
+
+(defn notify-clients [msg]
+  (doseq [channel @channels]
+    (send! channel msg)))
+
+;; * WebSocket Handler
+(defn websocket-handler [request]
+  (with-channel request channel
+    (connect! channel)
+    (on-close channel (partial disconnect! channel))
+    (on-receive channel #(notify-clients %))))
+
+;; * Main
 (defn home-page [request]
   (layout/render request "home.html"))
 ;; * Request Helpers
@@ -103,10 +131,27 @@
               (catch Exception e
                 (str (.getCause e))))))}}]
 
-
+;; * WebSocket Routes
+   ["/ws" websocket-handler]
 ;; * Come with Luminus, just docs
    ["/docs" {:get (fn [_]
                     (-> (response/ok (-> "docs/docs.md" io/resource slurp))
                         (response/header "Content-Type" "text/plain; charset=utf-8")))}]])
+
+
+;; * Event Listener
+(mount/defstate channels
+  :start (atom #{}))
+
+(mount/defstate ^{:on-reload :noop} event-listener
+  :start (db/add-listener
+          db/notifications-connection
+          :events
+          (fn [_ _ message]
+            (doseq [channel @channels]
+              (http/send! channel message))))
+  :stop (db/remove-listener
+         db/notifications-connection
+         event-listener))
 
 
